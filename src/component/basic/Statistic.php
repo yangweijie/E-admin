eae85d2fa974c3bea639f05054106e1921e70cb7
rocky <631670747@qq.com>
2021-09-08 17:39:37 +0800

    新增卡片统计样式

--- src/component/basic/Statistic.php
+++ src/component/basic/Statistic.php
@@ -5,6 +5,7 @@ namespace Eadmin\component\basic;
 
 
 use Eadmin\component\Component;
+use Eadmin\constant\Style;
 
 class Statistic extends Component
 {
@@ -74,4 +75,38 @@ class Statistic extends Component
 	{
 		return new static($title, $value, $icon, $bgColor1, $bgColor2);
 	}
+
+    /**
+     * 统计卡片
+     * @param string $text 文本
+     * @param string|int $value 值
+     * @param string $icon 图标
+     * @param string $color 颜色
+     * @return Card
+     */
+	public static function card($text,$value,$icon,$color='#409eff'){
+        return Card::create([
+            Html::create()
+                ->tag('i')
+                ->attr('class', $icon)
+                ->style([
+                    'fontSize' => '50px',
+                    'color'=>$color,
+                ]),
+            Html::create([
+                Html::create($text)
+                    ->tag('div')
+                    ->style([
+                        'fontSize'     => '14px',
+                        'marginBottom' => '4px',
+                    ]),
+                Html::create($value)
+                    ->tag('div')
+                    ->style([
+                        'fontSize'   => '24px',
+                        'lineHeight' => '24px',
+                    ]),
+            ])->tag('div')->style(['marginLeft'=>'10px'])
+        ])->bodyStyle(Style::FLEX_CENTER);
+    }
 }
