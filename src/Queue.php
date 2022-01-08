7785dbf8590bb8b1d9033c6d17b9aa5a325cc620
rocky <631670747@qq.com>
2022-01-08 15:03:01 +0800

    队列新增失败执行方法

--- src/Queue.php
+++ src/Queue.php
@@ -21,7 +21,7 @@ abstract class Queue
     protected $queueId = 0;
     protected $time;
     protected $queue;
-    protected $retry = 0;
+    protected $tries = 0;
     public function init($job)
     {
         $this->time = microtime(true);
@@ -167,8 +167,12 @@ abstract class Queue
         }
         $this->error('<b style="color: red">任务失败</b>');
         //任务重试
-        if($this->retry > 0 && $this->job->attempts() <= $this->retry){
+        if($this->tries > 0 && $this->job->attempts() <= $this->tries){
             $this->release();
+        }else{
+            if(method_exists($this,'fail')){
+                $this->fail($data);
+            }
         }
     }
 }
